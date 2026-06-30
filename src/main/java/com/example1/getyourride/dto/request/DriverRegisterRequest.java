de04f200caf77540e067dc3695eddd58ea20786e
younglee <kelvinyoungee47@gmail.com>
2026-06-30 17:12:25 +0200

    loging in works for now

---
+++ src/main/java/com/example1/getyourride/dto/request/DriverRegisterRequest.java
@@ -0,0 +1,29 @@
+package com.example1.getyourride.dto.request;
+
+import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.NotBlank;
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+public class DriverRegisterRequest {
+
+    @NotBlank(message = "First name is required")
+    private String firstName;
+
+    @NotBlank(message = "Last name is required")
+    private String lastName;
+
+    @NotBlank(message = "Email is required")
+    @Email(message = "Invalid email format")
+    private String email;
+
+    private String phone;
+
+    @NotBlank(message = "Password is required")
+    private String password;
+
+    @NotBlank(message = "Role is required")
+    private String role; // "STUDENT_DRIVER" or "SHUTTLE_DRIVER"
+}
